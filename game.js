dc292565cc1dfd01195a0bf1699944f201a80126
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2021-07-08 11:27:34 +0530

    Add digger map generation and drawing

--- game.js
+++ game.js
@@ -37,14 +37,30 @@ async function run() {
   }
   createEntity.id = 0
 
-  const visuals = {
-    player: ['@', 'hsl(60, 100%, 50%)'],
-    troll: ['T', 'hsl(120, 60%, 50%)'],
-    orc: ['o', 'hsl(100, 30%, 50%)'],
-  }
-
   const player = createEntity('player', 5, 4)
   const troll = createEntity('troll', 20, 10)
+
+  const createMap = (width, height) => {
+    const map = {
+      width,
+      height,
+      tiles: new Map(),
+      key(x, y) {
+        return `${x},${y}`
+      },
+      get(x, y) {
+        return this.tiles.get(this.key(x, y))
+      },
+      set(x, y, value) {
+        this.tiles.set(this.key(x, y), value)
+      },
+    }
+
+    const digger = new ROT.Map.Digger(width, height)
+    digger.create((x, y, contents) => map.set(x, y, contents))
+    return map
+  }
+  const map = createMap(80, 60)
 
   /* Process */
 
@@ -64,12 +80,33 @@ async function run() {
     }
   }
 
+  /* Draw */
+
+  const drawMap = (map) => {
+    for (let y = 0; y < map.height; y++) {
+      for (let x = 0; x < map.width; x++) {
+        if (map.get(x, y)) {
+          display.draw(x, y, '⨉', 'hsl(60, 10%, 40%)', 'gray')
+        } else {
+          display.draw(x, y, '·', 'hsl(60, 50%, 50%)', 'black')
+        }
+      }
+    }
+  }
+
+  const entityVisuals = {
+    player: ['@', 'hsl(60, 100%, 50%)'],
+    troll: ['T', 'hsl(120, 60%, 50%)'],
+    orc: ['o', 'hsl(100, 30%, 50%)'],
+  }
   const drawEntity = (entity) => {
-    const [ch, fg, bg] = visuals[entity.type]
+    const [ch, fg, bg] = entityVisuals[entity.type]
     display.draw(entity.x, entity.y, ch, fg, bg)
   }
+
   const draw = () => {
     display.clear()
+    drawMap(map)
     for (const entity of entities.values()) {
       drawEntity(entity)
     }
